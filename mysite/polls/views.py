3acf3111a6753f268ea2e5b9f2abcaf0b133b407
zhengt <tony_zheng35@hotmail.com>
2017-03-23 10:03:54 +0000

    template rendering in polls index.html and detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from django.http import HttpResponse
 
@@ -8,17 +8,22 @@ from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-        })
-    return HttpResponse(template.render(context))
+    # template = loader.get_template('polls/index.html')
+    # context = RequestContext(request, {
+        #'latest_question_list': latest_question_list,
+        #}) once template loaded you no longer need loader, RequestContext and HttpResponse, unless you still use the function as below
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    # return HttpResponse(template.render(context))
     # output = ', '.join([p.question_text for p in latest_question_list])
     # return HttpResponse(output)
     # return HttpResponse("Hello, world. You're at the polls index")
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    # return HttpResponse("You're looking at question %s." % question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
